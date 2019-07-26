7104b2da4e1572fcdbd5684d9826cf2a15663dd2
Binh Trinh <trinhthaibinh.ecom@gmail.com>
2019-07-26 11:16:59 -0700

    Sign in and sign out with firebase

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,7 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { withRouter } from 'react-router-dom';
+import { compose } from 'recompose';
 
-const Signin = () => {
-  return <div>Signin</div>;
+import { SignUpLink } from '../SignUp';
+import { withFirebase } from '../Firebase';
+import * as ROUTES from '../../constants/routes';
+
+const SignInPage = () => (
+  <div>
+    <h1>Sign In</h1>
+    <SignInForm />
+    <SignUpLink />
+  </div>
+);
+
+const INITIAL_STATE = {
+  email: '',
+  password: '',
+  error: null
 };
 
-export default Signin;
+const SignInFormBase = ({ firebase, history }) => {
+  const [formData, setFormData] = useState({ ...INITIAL_STATE });
+  const onSubmit = async e => {
+    e.preventDefault();
+    const { email, password } = formData;
+
+    try {
+      await firebase.doSignInWithEmailAndPassword(email, password);
+      setFormData({ ...INITIAL_STATE });
+      history.push(ROUTES.HOME);
+    } catch (err) {
+      console.log(err);
+      setFormData({ ...formData, error: err });
+    }
+  };
+
+  const onChange = e => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const { email, password, error } = formData;
+
+  const isInvalid = password === '' || email === '';
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        name="email"
+        value={email}
+        onChange={onChange}
+        type="text"
+        placeholder="Email Address"
+      />
+      <input
+        name="password"
+        value={password}
+        onChange={onChange}
+        type="password"
+        placeholder="Password"
+      />
+      <button disabled={isInvalid} type="submit">
+        Sign in
+      </button>
+      {error && <p>{error.message}</p>}
+    </form>
+  );
+};
+
+const SignInForm = compose(
+  withRouter,
+  withFirebase
+)(SignInFormBase);
+
+export default SignInPage;
+
+export { SignInForm };
