a190be822ac607be208d4917d8406ee7fcbe1879
Binh Trinh <trinhthaibinh.ecom@gmail.com>
2019-07-26 10:21:11 -0700

    Set up React router with routes

---
+++ src/components/Account/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const index = () => {
+  return <div>Account</div>;
+};
+
+export default index;
---
+++ src/components/PasswordForget/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const index = () => {
+  return <div>Password Forget</div>;
+};
+
+export default index;
